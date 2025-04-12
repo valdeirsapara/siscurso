dbfae42d47d51eb152a602d4a4bf3d78e0f35c27
Valdeir Sampaio Sapará Filho <saparavaldeir@gmail.com>
2025-04-12 19:05:19 -0400

    trocando config.boolean para bool para evitar erro de typos no compilador do projeto

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY",default="6XnEXdNOWE")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=True, cast=config.boolean)
+DEBUG = config("DEBUG", default=True, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
 
 
