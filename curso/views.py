bafefea01464f11237bd3cfd584829517644182c
Lariszac <larissacls00@gmail.com>
2025-05-24 16:24:27 -0300

    Iniciando o Front

--- curso/views.py
+++ curso/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Curso
+
+def listar_cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'cursos/lista.html', {'cursos': cursos})
+
+def detalhe_curso(request, curso_id):
+    curso = get_object_or_404(Curso, id=curso_id)
+    return render(request, 'cursos/detalhe.html', {'curso': curso})
