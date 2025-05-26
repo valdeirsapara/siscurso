42480aaaf3f1f85f0fd07991dafe210ae15ca5cd
Valdeir Sampaio Sapará Filho <saparavaldeir@gmail.com>
2025-05-26 16:09:37 -0400

    implementa novas funocnalidades a model de curso

--- curso/admin.py
+++ curso/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Curso
+from core.admin import AbstractAdmin
 
 # Register your models here.
 
-admin.site.register(Curso)
+class CursoAdmin(AbstractAdmin):
+    pass
+    
+
+admin.site.register(Curso, CursoAdmin)
--- curso/models.py
+++ curso/models.py
@@ -3,16 +3,19 @@ from django.db import models
 
 # third parties
 from polymorphic.models import PolymorphicModel
+
 # local 
+from core.models import AbstractMixin
 from contrib.models import Aluno, Professor
 
+
 # Create your models here.
-class Curso(PolymorphicModel):
+class Curso(PolymorphicModel, AbstractMixin):
     name = models.CharField(max_length=255) # campo obrigatorio
-    descripition = models.TextField()
+    description = models.TextField()
     duracao = models.PositiveIntegerField()  # duração em horas
     # modulos = models.ManyToManyField('Modulo', related_name='cursos')
-    alunos = models.ManyToManyField(Aluno, related_name="cursos", blank=True, null=True)
+    alunos = models.ManyToManyField(Aluno, related_name="cursos", blank=True,)
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name="cursos",)
     data_de_criação = models.DateTimeField(auto_now_add=True,)
     ativo = models.BooleanField(default=True) 
@@ -26,10 +29,9 @@ class Gratuito(Curso):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        self.save()
 
     def __str__(self) -> str:
-        return f"Gratuito: {self.nome} | {self.professor.user.get_full_name()}"
+        return f"Gratuito: {self.name} | {self.professor.user.get_full_name()}"
             
 
 class Pago(Curso):
