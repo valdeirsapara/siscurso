b5ec814d33be1dfd173b710e6826ddf806b50fe3
Valdeir Sampaio Sapará Filho <saparavaldeir@gmail.com>
2025-05-26 16:08:26 -0400

    adciona depência para pegar o use atual

--- config/settings.py
+++ config/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_currentuser.middleware.ThreadLocalUserMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
